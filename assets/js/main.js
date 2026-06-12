2e11fd0db88ceb32d12176b04760dfd573805c56
Scathyo <Patrick.Sticht@web.de>
2026-06-12 04:26:26 +0200

    Update main.js
    added hamburger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-// Hauptskript
+// Hamburger Button auswählen
+const hamburger = document.querySelector(".hamburger");
+
+// Navigation auswählen
+const navLinks = document.querySelector(".nav-links");
+
+// Klick auf Hamburger
+hamburger.addEventListener("click", () => {
+
+   // Klasse active hinzufügen/entfernen
+   navLinks.classList.toggle("active");
+
+});
